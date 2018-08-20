271566be605da604985189b7a5d65baff151eb49
André Lademann <vergissberlin@googlemail.com>
2018-08-20 22:56:05 +0200

    Added properties

--- js/planner.js
+++ js/planner.js
@@ -9,18 +9,27 @@ $(function() {
 
 
   $('#cmd').click(function() {
-var doc = new jsPDF();
+    var doc = new jsPDF();
 
-var specialElementHandlers = {
- '#editor': function(element, renderer){
-  return true;
- }
-};
+    var specialElementHandlers = {
+     '#editor': function(element, renderer){
+      return true;
+     }
+    };
 
-doc.fromHTML($('body').get(0), 15, 15, {
- 'width': 170,
- 'elementHandlers': specialElementHandlers
-});
+    doc.setProperties({
+     title: 'Title',
+     subject: 'This is the subject',
+     author: 'Author Name',
+     keywords: 'generated, javascript, web 2.0, ajax',
+     creator: 'Creator Name'
+    });
+
+
+    doc.fromHTML($('body').get(0), 15, 15, {
+     'width': 170,
+     'elementHandlers': specialElementHandlers
+    });
     doc.save('sample-file.pdf');
   });
 });
